2fb549de2c623eada92c8e985e8e4dc8c4d7f3b0
jepbura <jepbura@gmail.com>
2024-03-15 16:16:44 +0330

    Add web site

--- web-site/src/components/pageTitle.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-export default function PageTitle({ title }) {
-  return (
-    <span className="text-bios-header font-bios3 absolute top-10 left-10 underline animate-fade-in-up">
-      {title}
-    </span>
-  )
-}
--- web-site/src/core/utils/props.js
+++
@@ -1,1 +0,0 @@
-export const getUpdateLink = "https://stoplight.io/mocks/bura/bura/78556887/get-update"
